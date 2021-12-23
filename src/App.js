9c2bc5e10bde7a1674dae2c55ba7527ac0041fe3
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-23 12:59:58 +0530

    fitst testing of sandbox fitst testing of sandbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { Component } from "react";
+class App extends Component {
+  render() {
+    return <div className="App">Hello World</div>;
+  }
 }
+export default App;
